88a151e4a48cf43b164d26c0ed331e391e721b8f
Erik Sierra <120680439+ErikSierra@users.noreply.github.com>
2024-05-14 10:27:51 -0400

    Create OutlookTest.py

---
+++ OutlookTest.py
@@ -0,0 +1,6 @@
+import win32com.client
+
+outlook = win32com.client.Dispatch('outlook.application')
+inbox = outlook.GetNamespace("MAPI").GetDefaultFolder(5)
+num_emails = inbox.Items.Count
+print(f"Number of emails in Inbox: {num_emails}")
